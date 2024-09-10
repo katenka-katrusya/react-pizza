9dff2c3a6685c03781e4fa83cf4f12683ea5af49
SEkaterina <katerina-kozyrenko@yandex.ru>
2024-09-10 08:29:51 +0300

    server on render

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:3000';
-// const BASE_URL = 'https://json-server-tvz0.onrender.com';
+// const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://pizzas-data.onrender.com';
 
 export const ALL_PIZZAS = `${BASE_URL}/items`;
 export const PIZZA_BY_ID = `${BASE_URL}/items/`;
